641247858c7451f0853c8bd7837b0015c9d1be2f
CodyLombardi <codyjlombardi@gmail.com>
2021-12-12 11:54:45 -0800

    Signed-off-by: CodyLombardi <codyjlombardi@gmail.com>

---
+++ Project2/Project 2 v2/Cards.cpp
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Cards.cpp
+ * Author: codyj
+ * 
+ * Created on December 12, 2021, 11:52 AM
+ */
+
+#include "Cards.h"
+
+Cards::Cards() {
+}
+
+Cards::Cards(const Cards& orig) {
+}
+
+Cards::~Cards() {
+}
+
